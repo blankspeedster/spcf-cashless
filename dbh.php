5a53b8a4d170ead2b32590d7b2a0715884735b5a
blankspeedster <ronie.bituin@trainocate.com>
2022-01-28 19:25:40 +0800

    :coffee: initial commit

---
+++ dbh.php
@@ -0,0 +1,25 @@
+<?php
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+$production = false;  
+
+if($production)
+{
+    $host = 'localhost';
+    $username = 'root';
+    $password = '';
+    $database = 'tlpmg';
+}
+else 
+{
+    $host = 'localhost';
+    $username = 'root';
+    $password = '';
+    $database = 'tlpmg';
+}
+
+$mysqli = new mysqli($host, $username, $password, $database) or die(mysqli_error($mysqli));
+
+?>
