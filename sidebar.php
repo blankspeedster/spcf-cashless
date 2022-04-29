d525a289cc9bc08021111bb42eda484292831a47
blankspeedster <ronie.bituin@trainocate.com>
2022-04-29 13:16:53 +0800

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -107,11 +107,11 @@ else if($_SESSION['role'] == "3"){
                     <span>Users</span></a>
             </li>
 
-            <li class="nav-item" style="display: <?php if($_SESSION['role']==3){echo "none;";} ?>;">
+            <!-- <li class="nav-item" style="display: <?php if($_SESSION['role']==3){echo "none;";} ?>;">
                 <a class="nav-link" href="all_transactions.php">
                     <i class="fas fa-fw fa-table"></i>
                     <span>Cash In and Cash Out Transactions</span></a>
-            </li>
+            </li> -->
 
             <!-- Nav Item - Transactions -->
             <li class="nav-item" style="display: none;">
